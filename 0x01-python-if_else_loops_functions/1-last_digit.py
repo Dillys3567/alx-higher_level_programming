c973f1791200e6d88ecf719458ea7ce3344f75b0
Dillys Annan <dillysannan@gmail.com>
2023-07-11 18:01:17 -0700

    pos neg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,8 @@ else:
 if lastdig == 0:
     print("Last digit of {} is {} and is 0".format(number, lastdig))
 elif lastdig > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastdig))
+    print("Last digit of {} is {} and is greater than 5".format(number, lastdig
+        ))
 elif lastdig < 6 and lastdig != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastdig))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number
+        , lastdig))
