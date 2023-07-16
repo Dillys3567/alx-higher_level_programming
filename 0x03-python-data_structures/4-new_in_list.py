112c7f7f3c46b610e8dd0544cdf946f43dcd8c3d
Dillys Annan <dillysannan@gmail.com>
2023-07-15 20:13:59 -0700

    copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return(my_list)
+    new = my_list.copy()
+    new[idx] = element
+    return new
+
