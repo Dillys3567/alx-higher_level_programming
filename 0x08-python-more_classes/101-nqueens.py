f23f1294ac3818af6a66ae3872cb41ca573b784d
Dillys Annan <dillysannan@gmail.com>
2023-08-07 17:19:49 -0700

    queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,65 +1,121 @@
 #!/usr/bin/python3
+"""Solve the nqueens puzzle.
 
-"""
-module for calculation of n-queens problem
-"""
+Args:
+    board (list): list represnting chessboard
+    solutions (list): list of solutions
 
+queen must be on chessboardd
+"""
 import sys
 
-class Solution_Board:
-    """class for solving queens problem
+def init_board(n):
+    """Initialize chessboard"""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+def board_deepcopy(board):
+    """return copy of board"""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+def get_solution(board):
+    """Return list of solved chessboard
     """
-    solutions = []
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
 
-    def __init__(self, num):
-        """initialise function
-        """
-        self.num = num
+def xout(board, row, col):
+    """X out spots on a chessboard.
 
-    @property
-    def num(self):
-        """getter for num
-        """
-        return self.__num
+    Args:
+    board (list): current chessboard.
+    row (int): row where queen was
+    col (int): column where queen was
+    """
+    #forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    #backward spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    #spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    #spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    #spots diagonal right down
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    #spots diagonal up left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    #spots diagonal right up
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    #spots diagonal to left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -=1
 
-    @num.setter
-    def num(self, value):
-        """setter for num
-        """
-        if not isinstance(num, int):
-            raise Typeerror("num should be an int")
-        self.__num = value
+def recursive_solve(board, row, queens, solutions):
+    """Recursive;y solve puzzle
+    Args:
+        board (list): current chessboard
+        row (int): current working row
+        queens (int): current number of queens
+    Returns:
+        solutions
+    """
+    if queens == len(board):
+        solutions.append(get_solution(board))
+        return (solutions)
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            xout(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
+    return (solutions)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
 
 
-args = sys.argv
-
-if len(args) != 2:
-    exit(1)
-if not args[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-num = int(args[1])
-if num < 4:
-    print("N must be at least 4")
-    exit(1)
-
-solutions = []
-board = [[0 for a in range(0, num)] for b in range(0, num)]
-running = True
-
-while running:
-    sol = get_n_queens(board)
-    solutions.append(sol)
-    running = False
-
-def get_n_queens(chess_board, column, num):
-    if column >= num:
-        return True
-    for i in range (0, num):
-        if board_safe(chess_board, column):
-            chess_board[i][column] = 1
-            if get_n_queens(chess_board, column + 1):
-                return True
-            board[i][column] = 0
-    return (False)
