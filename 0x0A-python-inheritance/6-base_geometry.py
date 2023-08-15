f7819b8eeeef0ca7c394c88730bf05119305612f
Dillys Annan <dillysannan@gmail.com>
2023-08-14 17:55:56 -0700

    public method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A class called
+BaseGeometry"""
+class BaseGeometry():
+
+    """a public instance method that
+    raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
