5b081094168faa036ec401608b9fb43c353ae22a
Dillys Annan <dillysannan@gmail.com>
2023-08-15 19:18:14 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,8 @@
 """write to a file."""
 
 def write_file(filename="", text=""):
+    """write to a file
+    and overwrite any exiting text
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
