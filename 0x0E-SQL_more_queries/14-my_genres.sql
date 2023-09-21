dcab077a6c82a9cfcee977cf020393a5fc7399bb
Dillys Annan <dillysannan@gmail.com>
2023-09-20 21:17:07 -0700

    joins

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- show all genres of Dexter 
+SELECT name FROM tv_genres LEFT JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter' GROUP BY name ORDER BY name ASC;
