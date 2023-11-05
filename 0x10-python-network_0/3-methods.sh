52afee37f961146910c19bbcddfe7681dec7c5ec
Dillys Annan <dillysannan@gmail.com>
2023-11-05 11:51:06 -0800

    pipe

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 #http options
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-url="$1"
-
-allow_methods=$(curl -s -X OPTIONS -I "$url" | grep "Allow:" | cut -d ' ' -f 2-)
-
-if [ -z "$allow_methods" ]; then
-	echo "No 'Allow' header found."
-else
-	echo "$allow_methods"
-fi
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
