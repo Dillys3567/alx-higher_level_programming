0f53df92913de20b8a5cb9ad2f0fd70cc44b5ebe
Dillys Annan <dillysannan@gmail.com>
2023-11-05 12:02:32 -0800

    status

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-url="$1"
-
-response=$(curl -I -s "$url")
-
-code=$(echo "$response" |grep -i "HTTP/" | awk '{print $2}')
-
-echo "$code"
+curl -L -s -X HEAD -w "%{http_code}" "$1"
