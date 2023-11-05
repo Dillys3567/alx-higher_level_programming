ff9481150a7a92c254d2a0aa21e7b2fd6e1b484a
Dillys Annan <dillysannan@gmail.com>
2023-11-05 12:10:05 -0800

    lines

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#json post
 curl -s -d "@$2" -H "Content-Type: application/json" -X POST "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 #catch me
-
-response=$(curl -s 0.0.0.0:5000/catch_me)
-
-message="${response#*You got me!}"
-message="${message%%</h1>*}"
-printf "%s\n" "message"
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
