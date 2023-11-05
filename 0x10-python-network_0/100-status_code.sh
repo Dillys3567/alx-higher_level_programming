82d09385d8101cbf20049fe80ce49966d582603c
Dillys Annan <dillysannan@gmail.com>
2023-11-05 12:05:33 -0800

    code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#display status code
 curl -L -s -X HEAD -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-if [ $# -ne 2 ]; then
-	echo "Usage: $0 <URL> <filename>"
-	exit 1
-fi
-
-url="$1"
-filename="$2"
-
-if [ ! -f "$filename" ]; then
-	echo "File '$filename' not found."
-	exit 1
-fi
-
-curl -s -X POST -H "Content-Type: application/json" --data "@filename" "$url"
+curl -s -d "@$2" -H "Content-Type: application/json" -X POST "$1"
