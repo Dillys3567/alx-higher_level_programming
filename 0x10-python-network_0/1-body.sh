b938f095e15ec5cdd551af3a6fae76be43078bb1
Dillys Annan <dillysannan@gmail.com>
2023-11-05 11:45:06 -0800

    lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 #display body of get request
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-url="$1"
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$response" -eq 200 ]; then
-	curl -s "$url"
-else
-	echo "$response"
-fi
+if [$(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
