ad26bee8951f4c9c66ea82dd0fb73bb957980abe
Dillys Annan <dillysannan@gmail.com>
2023-11-05 12:01:00 -0800

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,12 @@
+#!/usr/bin/python3
+"""script for finding peak in list of ints
+"""
+
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return None
-
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-    return list_of_integers[left]
+    """forced implementation
+    """
+    maxs = None
+    for i in list_of_integers:
+        if maxs is None or maxs < i:
+            maxs = i
+    return maxs
