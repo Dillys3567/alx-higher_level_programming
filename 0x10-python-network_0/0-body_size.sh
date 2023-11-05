e17ae1305761d5bc79d6943483132807d7b76c9f
Dillys Annan <dillysannan@gmail.com>
2023-11-05 11:42:09 -0800

    lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 #Bash script takes URL, sends request,displays size of the body
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url="$1"
-
-response=$(curl -sI "$url")
-
-content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$content_length" ]; then
-	echo "Content-Length header not found."
-	exit 1
-fi
-
-echo "${content_length}"
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
