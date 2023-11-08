c8146dcc78e97bc6e84b1722b967f05e427401d0
Dillys Annan <dillysannan@gmail.com>
2023-11-07 16:04:20 -0800

    c

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const v1 = ['Ci is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(v1[0] + '\n' + v1[1] + '\n' + v1[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
